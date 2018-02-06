a8e3c84b43ff4d4b2114d83f38c5ee32873eaf38
Javier Revillas <jrevillas@massivedynamic.io>
2018-02-06 01:32:23 +0100

    Add support for listing functions
    Signed-off-by: Javier Revillas <jrevillas@massivedynamic.io>

--- gateway/assets/js/faas-ui.js
+++ gateway/assets/js/faas-ui.js
@@ -1,1 +1,12 @@
-angular.module('faasUI', ['ngMaterial']);
+angular
+  .module('faasUI', ['ngMaterial'])
+  .controller('faasCtrl', function($mdSidenav, $scope) {
+    $scope.avatar = function(func) {
+      return 'https://www.gravatar.com/avatar/' + func.name + '?d=identicon&f=y';
+    };
+    $scope.functions = [
+      {image: 'jrevillas/faasfriday', invocations: '1.337', name: 'faasfriday'},
+      {image: 'jrevillas/applepay', invocations: '125', name: 'applepay'}
+    ];
+    console.log($scope.functions);
+  });
